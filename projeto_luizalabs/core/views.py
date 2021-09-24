8e28d05f97761f3a7775a536268c9dc91436cfa8
Val Araujo <val.araujo@businessplanner.com.br>
2021-09-24 10:33:22 -0300

    Mudanca layout tela inicial

--- projeto_luizalabs/core/views.py
+++ projeto_luizalabs/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "base.html")
+    return render(request, "luizalabs.html")
 
 def dashboard(request):
     return render(request, "luizalabs.html")
