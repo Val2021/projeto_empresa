67bb90af310e212ff20a0a53e5f574599039f081
Val Araujo <val.araujo@businessplanner.com.br>
2021-09-24 10:12:37 -0300

    Finalizado metodo delete_produto_empresa

--- projeto_luizalabs/empresa/views/produto_view.py
+++ projeto_luizalabs/empresa/views/produto_view.py
@@ -34,13 +34,12 @@ def produto_empresa(request,empresaID):
 def delete_produto_empresa(request,empresaID,view=None):
     empresa=do_read_empresa_by_id(empresaID)
     empresa["id"] = str(empresa["_id"])
-    print("empresa",empresa)
     produtos = empresa["produtos"]
     for id_produto in produtos:
         for produto in empresa["produtos"]:
-            if id_produto == produto["id_produto"]: #precisa terminar
+            if id_produto["id_produto"] == produto["id_produto"]: 
                 empresa["produtos"].remove(produto)
         do_update_empresa(empresaID,empresa)
-    return redirect("empresa:produto_empresa")
+    return redirect("empresa:produto_empresa", empresaID)
 
     
